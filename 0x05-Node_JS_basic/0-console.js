cca61d0fbeff3b4c565353268184b783ad6d920e
azukauteh <aazure263@gmail.com>
2024-02-26 12:48:09 +0000

    Execute basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Prints a string to the standard output
+ * @param {String} message - The string to be printed
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
