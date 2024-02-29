d26033cd40e81197ce239e36867291deef368fd3
azukauteh <aazure263@gmail.com>
2024-02-29 18:16:35 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,26 @@
+
+
+/**
+ * Round params and perform arithmetic operation based on the type
+ * @param {string} type - The type of operation (SUM, SUBTRACT, DIVIDE)
+ * @param {number} a - The first number
+ * @param {number} b - The second number
+ * @returns {number|string} - The result of the operation or an error message
+ */
+function calculateNumber(type, a, b) {
+  let result;
+  if (type === 'SUM') {
+    result = Math.round(a) + Math.round(b);
+  } else if (type === 'SUBTRACT') {
+    result = Math.round(a) - Math.round(b);
+  } else if (type === 'DIVIDE') {
+    if (Math.abs(Math.round(b)) === 0) {
+      result = 'Error';
+    } else {
+      result = Math.round(a) / Math.round(b);
+    }
+  }
+  return result;
+}
+
+module.exports = calculateNumber;
