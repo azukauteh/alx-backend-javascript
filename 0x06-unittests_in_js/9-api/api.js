fe938600956210b9e9bf69378ce7d8e57f0dbcb4
azukauteh <aazure263@gmail.com>
2024-03-01 16:37:17 +0000

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -3,7 +3,6 @@
  */
 
 const express = require('express');
-
 const app = express();
 const port = 7865;
 
@@ -16,7 +15,6 @@ app.get('/cart/:id([0-9]+)', (req, res) => {
   const cartId = req.params.id;
   res.send(`Payment methods for cart ${cartId}`);
 });
-
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
