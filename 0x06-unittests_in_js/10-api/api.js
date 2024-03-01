946e9c0f3268a171e4c32d6821ae78f3984df12b
azukauteh <aazure263@gmail.com>
2024-03-01 17:29:19 +0000

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,31 +3,30 @@
  */
 
 const express = require('express');
+
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
-// New endpoint for cart with ID validation
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  const cartId = req.params.id;
-  res.send(`Payment methods for cart ${cartId}`);
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
 });
 
-// New endpoint for available payments
-app.get('/available_payments', (req, res) => {
-  const paymentMethods = {
+app.get('/available_payments', (_req, res) => {
+  res.json({
     payment_methods: {
       credit_cards: true,
-      paypal: false
-    }
-  };
-  res.json(paymentMethods);
+      paypal: false,
+    },
+  });
 });
 
-// New endpoint for login
 app.post('/login', (req, res) => {
   const { userName } = req.body;
   res.send(`Welcome ${userName}`);
