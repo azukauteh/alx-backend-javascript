b3773d38fb5d648e5a021f3caa20e704c19d5646
azukauteh <aazure263@gmail.com>
2024-03-01 13:09:03 +0000

    Basic Integration testing

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,26 @@
+
+/**
+ * api.test.js
+ *
+ */
+
+
+const request = require('supertest');
+const app = require('./api');
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Index page', () => {
+  it('responds with status code 200 and correct result', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
+});
+
